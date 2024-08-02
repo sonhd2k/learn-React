6ff44d488174fbfa6301afac8542541bc307c0c8
sonhd2k <sonhd2k@gmail.com>
2024-08-02 12:53:36 +0700

    video 16

---
+++ src/Compodents/Myconpodent.js
@@ -0,0 +1,12 @@
+// Class compodent
+// Function compodent
+import React from "react";
+class Myconpodent extends React.Component {
+  //JSX
+   render(){
+    return(
+      <div>Nhung {Math.random()}</div>
+    );
+   }
+}
+export default Myconpodent;
