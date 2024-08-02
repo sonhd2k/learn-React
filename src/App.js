ec9ff21ae6f8aab07473f9d7afd0b8e404b901cc
sonhd2k <sonhd2k@gmail.com>
2024-08-03 05:18:00 +0700

    video 17

--- src/App.js
+++ src/App.js
@@ -3,28 +3,38 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import Myconpodent from './Compodents/Myconpodent';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react';
 
-  return (
-    <div>
+class App extends React.Component{
+  render(){
+    return(
+      <div>
       hello
       <Myconpodent/>
     </div>
+    );
+  }
+} 
+// const App = () => {
+//   const count = useSelector(state => state.counter.count);
+//   const dispatch = useDispatch();
 
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <div>Count = {count}</div>
-    //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-    //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-    //   </header>
-    // </div>
-  );
-}
+//   return (
+ 
+
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <div>Count = {count}</div>
+//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+//     //   </header>
+//     // </div>
+//   );
+// }
+
 
 export default App;
--- src/Compodents/Myconpodent.js
+++ src/Compodents/Myconpodent.js
@@ -2,10 +2,20 @@
 // Function compodent
 import React from "react";
 class Myconpodent extends React.Component {
+  state={
+    name:'Sơn',
+    address:'Hà Nội',
+    age:23
+  };
   //JSX
    render(){
     return(
-      <div>Nhung {Math.random()}</div>
+      <>
+      <div>Tên tôi là {this.state.name}</div>
+      <div>Địa chỉ của tôi là {this.state.address}</div>
+      <div>Tuổi tôi là {this.state.age}</div>
+      </>
+
     );
    }
 }
