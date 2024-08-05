a9344f96cf87f5ec2b87e6c0ccbefb53ad77b2d7
sonhd2k <sonhd2k@gmail.com>
2024-08-05 23:10:47 +0700

    video 24

--- src/Compodents/Dissplayinfor.js
+++ src/Compodents/Dissplayinfor.js
@@ -3,12 +3,21 @@ import React from "react";
 class Dissplayinfor extends React.Component{
  render(){
   //props=> viết tắt properties
-  console.log(this.props);
+  // const {age,name}=this.props;
+  const {listUsers}=this.props;
+  console.log(listUsers);
   return(
     <div>
-      <div> My name's {this.props.name}</div>
-      <div> My age's {this.props.age}</div>
-      
+      {/* <div> My name's {name}</div>
+      <div> My age's {age}</div> */}
+     {listUsers?.map((item,index) => {
+      return(
+        <div key={item.id}>
+          <div>tên tôi là {item.name}</div>
+          <div>tuổi tôi là {item.age}</div>
+        </div>
+      )
+     })}
        </div>
   )
 }
--- src/Compodents/Myconpodent.js
+++ src/Compodents/Myconpodent.js
@@ -5,13 +5,25 @@ import UserInfor from "./UserInfor";
 import Dissplayinfor from "./Dissplayinfor";
 class Myconpodent extends React.Component {
 
+  state={
+    listUsers:[
+      {id:1,name:"Sơn" ,age:"23"},
+      {id:2,name:"Nhung" ,age:"23"},
+      {id:3,name:"Nam" ,age:"27"},
+    ]
+  }
   //JSX
    render(){
+    const myInfor=['a','b','c']
     return(
       <div>
      <UserInfor></UserInfor>
      <br></br>
      <Dissplayinfor  name="Sơn" age="23"></Dissplayinfor>
+     <hr></hr>
+     <Dissplayinfor  name={'Nhung'} age={23} myInfor={myInfor}></Dissplayinfor>
+     <hr></hr>
+     <Dissplayinfor listUsers={this.state.listUsers}></Dissplayinfor>
       
       </div>
 
